4234122b18ccb1372718f0bfb285bd0bbf0d930d
DIDIERSimon <simon.didier200@gmail.com>
2021-03-22 11:27:02 +0100

    modification du controleur "Détail"

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -2,105 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Detail as ResourcesDetail;
-use App\Http\Controllers\BaseAuthController as BaseController;
 use App\Models\Detail;
-use Dotenv\Validator;
 use Illuminate\Http\Request;
+use App\Http\Controllers\JsonResponse;
 
-class DetailController extends BaseController
+class DetailController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $details =  Detail::all();
-        return $this->sendResponse(ResourcesDetail::collection($details), 'Affichage des details avec succès');
+        return Detail::all();
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show($det)
     {
-        $input = $request->all();
-
-        $validator = Validator::make($input, [
-            'nom' => 'required',
-            'description' => 'required',
-            'ingredients' => 'required',
-            'prix' => 'required',
-            'img_path' => 'required',
-        ]);
-        if($validator->fail()){
-            return $this->sendError('Erreur lors de la validation !', $validator->errors());
-        }
-        $details = Detail::create($input);
-        return $this->sendResponse(new ResourcesDetail($details), "Détails ajouté avec succès !");
+        return Detail::findOrFail($det);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($details)
+    public function store(Request $request): JsonResponse
     {
-        $details = Detail::findOrFail($details);
-        if(is_null($details)){
-            return $this->sendError('Details introuvable dans la base d\'information !');
-        }
-        return $this->sendResponse(new ResourcesDetail($details), "Détail trouver avec succès");
+        $det = Detail::create($request->all());
+        return response()->json($det, 201);
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $details)
+    public function update(Request $request, Detail $det)
     {
-        $input = $request->all();
-        $validator = Validator::make($input,[
-            'nom' => 'required',
-            'description' => 'required',
-            'ingredients' => 'required',
-            'prix' => 'required',
-            'img_path' => 'required',
-        ]);
-        if($validator->fails())
-        {
-            return $this->sendError('Impossible d\'update le plat !', $validator->errors());
-        }
-        $details->nom = $input['nom'];
-        $details->description = $input['description'];
-        $details->ingredients = $input['ingredients'];
-        $details->prix = $input['prix'];
-        $details->img_path = $input['img_path'];
-        $details->save();
-        return $this->sendResponse(new ResourcesDetail($details), 'Plat modifier avec succès !');
+        $det->update($request->all());
+        return response()->json($det, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($details)
+    public function delete(Detail $det)
     {
-        $details->delete();
-        return $this->sendResponse([], 'Plat supprimé avec succès !');
+        $det->delete();
+        return response()->json(null, 204);
     }
 }
